bc69a0578dd92d6784a1f6967c629501d570e74e
lai2-24 <lai2-24@itismeucci.com>
2025-12-03 13:47:16 +0100

    aggiustato

--- src/main/java/com/example/Miothread.java
+++ src/main/java/com/example/Miothread.java
@@ -12,13 +12,13 @@ import java.net.Socket;
 
 public class Miothread extends Thread {
 
-    Socket s; 
+    Socket s;
 
-    public Miothread (Socket s ){
-        this.s = s; 
+    public Miothread(Socket s) {
+        this.s = s;
     }
 
-                private static String getContentType(String path) {
+    private static String getContentType(String path) {
         if (path.endsWith(".html") || path.endsWith(".htm")) {
             return "text/html";
         } else if (path.endsWith(".css")) {
@@ -36,68 +36,33 @@ public class Miothread extends Thread {
         }
     }
 
-    public void run(){
-       BufferedReader in = null;
+    public void run() {
+        BufferedReader in = null;
         PrintWriter out = null;
 
-       try {
+        try {
             in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-       } catch (IOException e) {
-        
-        e.printStackTrace();
-       } 
-        try {
             out = new PrintWriter(s.getOutputStream(), true);
-        } catch (IOException e) {
-            
-            e.printStackTrace();
-        } 
-        try {
+            DataOutputStream outBinary = new DataOutputStream(s.getOutputStream());
+
             String firstline = in.readLine();
             System.out.println(firstline);
             String path = firstline.split(" ")[1];
             String method = firstline.split(" ")[0];
             if (!method.equals("GET")) {
-                out.println("HTTP/1.1 405 Method Not Allowed");
+                out.println("HTTP/1.0 405 Method Not Allowed");
                 s.close();
                 return;
             }
-            if (path.equals("/")) {
+            if (path.equals("/"))
                 path = "/index.html";
-            }else if (path.contains("..")) {
-                out.println("HTTP/1.1 400 Bad Request");
-                s.close();
-                return;
-            }else if (path.equals("")) {
-                path = "/index.html";
-            }
-            else if (path.startsWith("/style.css")) {
-                path = "/style.css/style.css";
-            }
-            else if (path.startsWith("/script.js")) {
-                path = "/script.js/script.js";
-            }
-            else if (path.startsWith("/images.png")) {
-                path = "/images.png/images.png";
-            }
-            else if (path.startsWith("/images.jpg")) {
-                path = "/images.jpg/images.jpg";
-            }
-            else if (path.startsWith("/images.jpeg")) {
-                path = "/images.jpeg/images.jpeg";
-            }
-            else if (path.startsWith("/images.gif")) {
-                path = "/images.gif/images.gif";
-            }
 
-            DataOutputStream outBinary = new DataOutputStream(s.getOutputStream());
             File file = new File("htdocs" + path);
-            if (file.exists()){
-                out.println("HTTP/1.1 200 OK");
+            if (file.exists()) {
+                out.println("HTTP/1.0 200 OK");
                 out.println("Content-Type:" + getContentType(path));
                 out.println("Content-Length: " + file.length());
                 out.println("");
-                out.println();
                 InputStream input = new FileInputStream(file);
                 byte[] buf = new byte[8192];
                 int n;
@@ -115,16 +80,14 @@ public class Miothread extends Thread {
                 out.flush();
 
             }
-            String h; 
+            String h;
             do {
                 h = in.readLine();
                 System.out.println(h);
             } while (!h.isEmpty());
-        } catch (IOException e) { 
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
-
-
-}    
-} 
+    }
+}
